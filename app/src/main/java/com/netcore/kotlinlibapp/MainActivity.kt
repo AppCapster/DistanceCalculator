0a7c9e320c9724b8a929384c2c8b60bb0eed6fe1
monika kale <monika.kale@netcore.co.in>
2019-07-08 15:04:28 +0530

    manifest add

---
+++ app/src/main/java/com/netcore/kotlinlibapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.netcore.kotlinlibapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.monika.distancecalculator.DistanceCalculator
+import com.monika.distancecalculator.Points
+
+class MainActivity : AppCompatActivity() {
+
+    val point1_lat = -34.6037389
+    val point1_long = -58.3815704
+    val point2_lat = 40.6892944
+    val point2_long = -74.0445004
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buenosAiresObeliscoPoint = Points(point1_lat, point1_long)
+        val nycStatueOfLibertyPoint = Points(point2_lat, point2_long)
+        val distanceBetweenPoints =
+            DistanceCalculator.calculateDistance(buenosAiresObeliscoPoint, nycStatueOfLibertyPoint)
+
+    }
+}
